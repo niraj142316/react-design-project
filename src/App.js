53078011a0516afeb7103635993ae30cab41d9ed
Niraj Kumar <104626456+niraj142316@users.noreply.github.com>
2024-06-10 23:16:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Reviews from './components/reviews/Reviews';
-// import Carousel from 'react-multi-carousel';
-// import 'react-multi-carousel/lib/styles.css';
-// import Sliders from './components/sliderContent/Sliders';
-// import {data, responsive} from './reviewData';
 import Home from './components/home/Home';
 import Blog from './components/blog/Blog';
 import Featured from './components/featured/Featured';
@@ -17,11 +13,6 @@ import Footer from './components/footer/Footer';
 
 function App() {
 
-    // const slider=data.map(item=>(
-    //   <Sliders name={item.name} star={item.star} desc={item.desc} profile={item.profile} iconTick={item.iconTick} title={item.title} icon={item.icon} tagline={item.tagline} subtag={item.subtag} />
-    // ))
-
-
   return (
     <div className="App">
       <Navbar />
@@ -30,9 +21,6 @@ function App() {
       <Featured />
       <div className="carosel">
       <Reviews />
-      // <Carousel showDots={true} infinite={true} responsive={responsive}>
-      //   {slider}
-      // </Carousel>
       </div>
       <Services />
       <Build />
